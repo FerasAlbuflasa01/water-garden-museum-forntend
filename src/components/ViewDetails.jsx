43a1042ea38ee5965e625c72d89c9efc3d76065e
abdkhalid24 <busaud973@gmail.com>
2025-08-05 10:36:09 +0300

    Setup the form of view details of the game

--- src/components/ViewDetails.jsx
+++ src/components/ViewDetails.jsx
@@ -1,4 +1,12 @@
 const ViewDetails = () => {
 
+    return (
+        <form className="details" >
+            <img src="https://media.alwasatnews.com/data/2016/5096/images/loc-8.jpg" alt="game.image" className="game-image" />
+            <h2 name={'game.name'}>Name</h2>
+            <h3 name={'game.price'}>Price</h3>
+            <p name={'game.discreption'}>Discreption</p>
+        </form>
+    )
 }
 export default ViewDetails
