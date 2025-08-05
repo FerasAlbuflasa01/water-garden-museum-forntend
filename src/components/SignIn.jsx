3e7e97752aac2c4e0f1ddd61daf88c885652da1a
abdkhalid24 <busaud973@gmail.com>
2025-08-05 09:19:48 +0300

    Sign in form is created

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,22 +1,38 @@
 import { useState } from "react"
+import { SignInUser } from '../services/Auth'
 import { useNavigate } from "react-router-dom"
 
 const SignIn = () => {
-    const {signIn, setSignIn} = useState(null)
-    
+    const initialState = { email: '', password: ''}
+    let navigate = useNavigate()
 
+    const [signIn, setSignIn]= useState(initialState)
+    
+    const handleChange = (e) => {
+        setSignIn({ ... signIn, [e.target.id]: e.target.value})
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        // const payload = await SignInUser(formValues)
+        setSignIn(initialState)
+        // setUser(payload)
+        navigate('/')
+      }
+    
     return (
-        <div className="sign-in">
+        <form className="sign-in" onSubmit={handleSubmit}>
         <h2>Sign in</h2>
         <label htmlFor="username">Username: </label>
-        <input type="text" name="username" placeholder="Enter your username" />
+        <input type="text" name="username" placeholder="Enter your username" onChange={handleChange}/>
         <br />
         <br />
         <label htmlFor="password">Password: </label>
-        <input type="text" name="password" placeholder="Enter your password" />
+        <input type="text" name="password" placeholder="Enter your password" onChange={handleChange}/>
+        <br />
         <br />
         <button name="submit">Submit</button>
-        </div>
+        </form>
     )
 }
 export default SignIn 
