bf4900651bd0e7eaf8fcdf028a9fff2c82f578a9
FerasGIT <ferasny@gmail.com>
2025-08-05 10:23:23 +0300

    update frontend

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,24 +1,14 @@
-import Client from './api'
+import Client, { BASE_URL } from './api'
 
+export const SignInUser = async (data) => {
+  try {
+    console.log(data)
+    const res = await Client.post(`http://localhost:3001/auth/login`, data)
+    // Set the current signed in users token to localStorage
+    localStorage.setItem('token', res.data.token)
 
-export const RegisterUser = async (data) => {
-    try {
-      const res = await Client.post('/auth/register', data)
-      return res.data
-    } catch (error) {
-      throw error
-    }
+    return res.data.user
+  } catch (error) {
+    throw error
   }
-  
-  export const SignInUser = async (data) => {
-    try {
-      const res = await Client.post('/auth/login', data)
-      // Set the current signed in users token to localStorage
-      localStorage.setItem('token', res.data.token)
-      
-      return res.data.user
-    } catch (error) {
-      throw error
-    }
-  }
-  
+}
