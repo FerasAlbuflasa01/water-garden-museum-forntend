51a82358b29d1e8e3a33c52133a01cd76435275e
alqaassass <ali.alqassas02@gmail.com>
2025-08-05 09:57:37 +0300

    Added (create game) to the front-end

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,4 +1,54 @@
-const New = () => {
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
+const New = () => {
+  const initialState = { name: '', price: '', image: '', description: '' }
+
+  let navigate = useNavigate()
+
+  const [creatGame, setCrreatGame] = useState(initialState)
+
+  const handleChange = (e) => {
+    setCrreatGame({ ...creatGame, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setCrreatGame(initialState)
+    navigate('/')
+  }
+
+  return (
+    <div>
+      <h1>Add A New Game Listing</h1>
+      <form className="add-game" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name={'name'}
+          placeholder={'name'}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name={'price'}
+          placeholder={'price'}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name={'img'}
+          placeholder={'image'}
+          onChange={handleChange}
+        />
+        <input
+          type="text-area"
+          name={'description'}
+          placeholder={'description'}
+          onChange={handleChange}
+        />
+        <button name='submit'>Submit</button>
+      </form>
+    </div>
+  )
 }
 export default New
