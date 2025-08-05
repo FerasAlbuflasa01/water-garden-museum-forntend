35112fcc4ecaaa9556843f5247d28fc947323fe8
FerasGIT <ferasny@gmail.com>
2025-08-05 09:31:11 +0300

    created login presetent

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import './App.css'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Home from './components/Home'
 import About from './components/About'
 import ViewGames from './components/ViewGames'
 import New from './components/New'
+import { useEffect, useState } from 'react'
+import Client, { BASE_URL } from './services/api'
 
 const App = () => {
+  const [admin, setAdmin] = useState(null)
+  const checkToken = async () => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      let admin = Client.get(`${BASE_URL}/auth/session`)
+      setAdmin(admin)
+    }
+  }
+  useEffect(() => {
+    checkToken()
+  }, [])
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/view/games' element={<ViewGames />} />
-      <Route path='/new/games' element={<New />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/view/games" element={<ViewGames />} />
+      <Route path="/new/games" element={<New />} />
     </Routes>
   )
 }
 
 export default App
-
